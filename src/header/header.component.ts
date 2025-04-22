c3445fa56bda0ec79e5c34690ccd98177d9b3da0
thepixelwriter <gauravsharma8813@gmail.com>
2025-04-22 17:28:51 +0530

    Added header component to display logo & app-name

---
+++ src/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    standalone: true,
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css',
+})
+
+export class HeaderComponent {
+
+}
